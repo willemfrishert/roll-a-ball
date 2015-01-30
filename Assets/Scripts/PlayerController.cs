efe19bd288d7c0875c3e2825b02a855055809bc5
Willem Frishert <willem@frishert.com>
2015-01-30 07:28:55 +0100

    Initial inbound
    Example code Project Roll-a-ball
    (http://unity3d.com/learn/tutorials/projects/roll-a-ball)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+	private int count;
+
+	void Start() {
+		count = 0;
+		this.UpdateCountText();
+		this.winText.text = "";
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rigidbody.AddForce(movement * this.speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);	
+			this.count++;
+			this.UpdateCountText();
+		}
+	}
+
+	void UpdateCountText() {
+		this.countText.text = "Count: " + count.ToString();
+
+		if (this.count >= 12)
+			this.winText.text = "You Win";
+	}
+}
